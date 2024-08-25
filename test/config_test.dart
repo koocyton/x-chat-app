a6255f3c0c9b07a991d567cb3176e2bffcf44d9d
liuyi <yi.liu@gameark.cn>
2024-08-25 21:45:48 +0800

    test api

--- test/config_test.dart
+++ test/config_test.dart
@@ -5,6 +5,9 @@ import 'package:flutter_test/flutter_test.dart';
 void main() {
 
   test('config test', (){
+    String requestHeader = "[{\"token\":\"bbb\"}]";
+    String requestUrl = "https://askfalfjfl.afaflsafa.dfd";
+    String requestRequestBody = "";
     String bodyKeyIndex = "data.0.body";
     // List<String> keyList = bodyKeyIndex.split(".");
     dynamic bodyResult = const JsonDecoder().convert("{\"data\": [{\"body\":\"hello2\"}]}");
